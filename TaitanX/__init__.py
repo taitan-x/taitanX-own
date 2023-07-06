c823bf68727bd94248536781974ad3179b97c1aa
taitan-x <138150134+taitan-x@users.noreply.github.com>
2023-07-06 19:28:04 +0530

    Add files via upload

---
+++ TaitanX/__init__.py
@@ -0,0 +1,37 @@
+from TaitanX.core.bot import AloneXBot
+from TaitanX.core.dir import dirr
+from TaitanX.core.git import git
+from TaitanX.core.userbot import Userbot
+from TaitanX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+app = TaitanXBot()
+
+userbot = Userbot()
+
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
